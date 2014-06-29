44b744fe896362c843b9e481004c533c16e7e73e
黄伟彬 <huangweibin-1991@163.com>
2014-06-29 11:19:58 +0800

    Creat New Project

---
+++ AWNavigationMenuViewDemo/AWNavigationMenuViewDemo/AWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AWAppDelegate.h
+//  AWNavigationMenuViewDemo
+//
+//  Created by 黄伟彬 on 14-6-29.
+//  Copyright (c) 2014年 Alan_Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AWNavigationMenuViewDemo/AWNavigationMenuViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AWNavigationMenuViewDemo
+//
+//  Created by 黄伟彬 on 14-6-29.
+//  Copyright (c) 2014年 Alan_Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AWAppDelegate class]));
+    }
+}
