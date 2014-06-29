6de865a657243984773d6a30e414d21be881279d
黄伟彬 <huangweibin-1991@163.com>
2014-06-29 11:49:32 +0800

    Creat Class named AWNavigationMenuButton

---
+++ AWNavigationMenuViewDemo/AWNavigationMenuViewDemo/AWNavigationMenu/AWNavigationMenuButton.h
@@ -0,0 +1,13 @@
+//
+//  AWNavigationMenuButton.h
+//  AWNavigationMenuViewDemo
+//
+//  Created by 黄伟彬 on 14-6-29.
+//  Copyright (c) 2014年 Alan_Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AWNavigationMenuButton : UIButton
+
+@end
---
+++ AWNavigationMenuViewDemo/AWNavigationMenuViewDemo/AWNavigationMenu/AWNavigationMenuButton.m
@@ -0,0 +1,13 @@
+//
+//  AWNavigationMenuButton.m
+//  AWNavigationMenuViewDemo
+//
+//  Created by 黄伟彬 on 14-6-29.
+//  Copyright (c) 2014年 Alan_Wong. All rights reserved.
+//
+
+#import "AWNavigationMenuButton.h"
+
+@implementation AWNavigationMenuButton
+
+@end
