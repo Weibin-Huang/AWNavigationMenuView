fee3ed4d248f62e32a5158d349be225887770269
AlanWong <alanwong@AlanWongs-iMac.local>
2014-07-03 14:59:53 +0800

    Delete repetitive Image source and Creat Class named AWNavigationMenuItem

---
+++ AWNavigationMenuViewDemo/AWNavigationMenuViewDemo/AWNavigationMenu/AWNavigationMenuItem.h
@@ -0,0 +1,13 @@
+//
+//  AWNavigationMenuItem.h
+//  AWNavigationMenuViewDemo
+//
+//  Created by AlanWong on 14-7-3.
+//  Copyright (c) 2014年 Alan_Wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AWNavigationMenuItem : NSObject
+
+@end
---
+++ AWNavigationMenuViewDemo/AWNavigationMenuViewDemo/AWNavigationMenu/AWNavigationMenuItem.m
@@ -0,0 +1,13 @@
+//
+//  AWNavigationMenuItem.m
+//  AWNavigationMenuViewDemo
+//
+//  Created by AlanWong on 14-7-3.
+//  Copyright (c) 2014年 Alan_Wong. All rights reserved.
+//
+
+#import "AWNavigationMenuItem.h"
+
+@implementation AWNavigationMenuItem
+
+@end
